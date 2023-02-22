60fc55bcd69f697242afccfddedd14a0e2dec9d5
Ricky Darmawan <rickydarmawan201@gmail.com>
2023-02-22 11:59:09 +0700

    test

--- js/main.js
+++ js/main.js
@@ -34,3 +34,14 @@ $(function() {
     
 
 });
+
+var icon =  document.getElementById("icon");
+icon.onclick = function(){
+ document.body.classList.toggle("dark-theme");
+ if(document.body.classList.contains("dark-theme")){
+	 icon.src = "images/kucing-surga.jpeg";
+ }
+ else{
+	 icon.src = "images/kucing-ngudud.jpg";
+ }
+}
